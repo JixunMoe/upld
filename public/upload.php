527ca2f3a94c5629e5f8ded7ea41222a656c57df
Jixun Wu <jixun.moe@gmail.com>
2019-01-23 18:04:02 +0000

    rework upload to work with multiple image upload

--- public/upload.php
+++ public/upload.php
@@ -5,6 +5,8 @@ require('common.php');
 
 validate_csrf();
 
+$url = empty($_POST['url']) ? null : $_POST['url'];
+
 // see if anonymous uploads has been disabled, and check if the user is logged in
 if (ANON_UPLOADS === false && !isset($_SESSION['user']))
 {
@@ -12,13 +14,13 @@ if (ANON_UPLOADS === false && !isset($_SESSION['user']))
 }
 
 // both image and url submitted. wtf, let's get the hell out of here!
-if (isset($_FILES['image']) && isset($_POST['url']))
+if (!empty($_FILES['image']) && !is_null($url))
 {
 	exit_message('Please only choose one image to upload.');
 }
 
 // neither submitted - inform user and exit
-if (!isset($_FILES['image']) && !isset($_POST['url']))
+if (empty($_FILES['image']) && is_null($url))
 {
 	exit_message('Please choose either an image on your computer to upload or a remote image to download.');
 }
@@ -29,229 +31,257 @@ $allowed_ext = [
 	'gif'
 ];
 
-// user must have submitted either an image or URL
-// check which one and make sure it's valid
-// check for an uploaded image first
-if (isset($_FILES['image']))
-{
-	// user wants to upload via browser
-	// set variables - will check after
-	$size = $_FILES['image']['size'];
-	$ext = pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
-}
-
-elseif (isset($_POST['url']))
-{
-	// user wants to download a remote image
-	// make sure URL is valid and set variables - will check after
-	// is remote downloading enabled in conf.php?
-	if (ALLOW_REMOTE !== true)
-	{
-		// remote downloading is disabled - error and exit
-		exit_message('Remote downloading is not enabled on this installation');
-	}
-
-	// allowed URL schemes
-	$allowed_schemes = [
-		'http',
-		'https'
-	];
-
-	// check if URL is valid and http/https only
-	if (!filter_var($_POST['url'], FILTER_VALIDATE_URL) || (!in_array(parse_url($_POST['url'], PHP_URL_SCHEME), $allowed_schemes)))
-	{
-		// not a valid URL
-		exit_message('Sorry, this URL is invalid');
-	}
-
-	// if whitelisting is enabled, make sure it's an allowed domain
-	if ((URL_WHITELIST === true) && (!in_array(parse_url($_POST['url'], PHP_URL_HOST), $allowed_urls)))
-	{
-		exit_message('Sorry, downloads from this domain have not been allowed by the administrator');
-	}
-
-	// looks good so far, download the image and make sure it's valid
-	$size = get_headers($_POST['url'], 1)['Content-Length'];
-	$ext = end(explode('.',$_POST['url']));
-}
-
-// OK, everything checks out so far
-// check size/ext
-if ($size > ALLOWED_SIZE)
-{
-	// file is too big
-	exit_message('Sorry, this file is too big');
-}
-
-// size is OK, make sure EXT is allowed
-if (!in_array($ext, $allowed_ext))
-{
-	// ext not allowed
-	exit_message('Sorry, this extension is not allowed.');
-}
-
-// size and ext are fine
-// let's set $image to either $_FILES['image'] or $_POST['url'] and check if they're valid
-if (isset($_FILES['image']))
-{
-	if (!getimagesize($_FILES['image']['tmp_name']))
-	{
-		exit_message('Sorry, this does not appear to be a valid image');
-	}
-
-	$image = $_FILES['image']['tmp_name'];
-}
-
-elseif (isset($_POST['url']))
-{
-	$image = file_get_contents($_POST['url'], NULL, NULL, NULL, $size);
-
-	if (!imagecreatefromstring($image))
-	{
-		exit_message('Sorry, this does not appear to be a valid image');
-	}
-}
-
-// everything looks good so far! images are valid, size and ext check out
-// generate an ID, move files and insert into DB
-
-// generate ID (and make sure it doesn't exist)
 require('db.php');
 
-// prepare query
-$exists = mysqli_prepare($db, 'SELECT EXISTS(SELECT 1 FROM `images` WHERE `id` = ?)');
+function process_upload($index, $url) {
+	global $allowed_ext;
+	global $db_queries;
+	global $db;
 
-// create ID and check if it exists in the DB
-do
-{
-	// create ID
-	$id = '';
-	$chars = 'ACDEFHJKLMNPQRTUVWXYZabcdefghijkmnopqrstuvwxyz23479';
-	for ($i = 0; $i < 5; ++$i)
+	// user must have submitted either an image or URL
+	// check which one and make sure it's valid
+	// check for an uploaded image first
+	if (!is_null($index))
 	{
-		$id .= $chars[mt_rand(0, 50)];
+		// user wants to upload via browser
+		// set variables - will check after
+		$size = $_FILES['image']['size'][$index];
+		$ext = pathinfo($_FILES['image']['name'][$index], PATHINFO_EXTENSION);
 	}
-	// $id is now set to a randomly generated ID
 
-	// query DB to see if ID exists
-	mysqli_stmt_bind_param($exists, "s", $id);
-	mysqli_stmt_execute($exists);
-	++$db_queries;
-	mysqli_stmt_bind_result($exists, $result);
-	mysqli_stmt_fetch($exists);
-	mysqli_stmt_close($exists);
-}
-while ($result === 1);
-
-// write image (this is different depending on whether it's an upload or remote download)
-if (isset($_FILES['image']))
-{
-	$image_path = 'images/' . $id . '.' . $ext;
-
-	// write image
-	move_uploaded_file($image, $image_path);
-}
-else if (isset($_POST['url']))
-{
-	// write image
-	file_put_contents('images/' . $id . '.' . $ext, $image);
-}
-
-// create thumbnail (only bother if user is logged in)
-if (isset($_SESSION['user']))
-{
-	if (isset($_FILES['image']))
+	elseif (!is_null($url))
 	{
-		// set source for thumb
-		switch ($ext)
+		// user wants to download a remote image
+		// make sure URL is valid and set variables - will check after
+		// is remote downloading enabled in conf.php?
+		if (ALLOW_REMOTE !== true)
 		{
-			case 'jpg':
-				$thumb = imagecreatefromjpeg($image_path);
-			break;
+			// remote downloading is disabled - error and exit
+			exit_message('Remote downloading is not enabled on this installation');
+		}
 
-			case 'png':
-				$thumb = imagecreatefrompng($image_path);
-			break;
+		// allowed URL schemes
+		$allowed_schemes = [
+			'http',
+			'https'
+		];
 
-			case 'gif':
-				$thumb = imagecreatefromgif($image_path);
-			break;
+		// check if URL is valid and http/https only
+		if (!filter_var($url, FILTER_VALIDATE_URL) || (!in_array(parse_url($url, PHP_URL_SCHEME), $allowed_schemes)))
+		{
+			// not a valid URL
+			exit_message('Sorry, this URL is invalid');
+		}
+
+		// if whitelisting is enabled, make sure it's an allowed domain
+		if ((URL_WHITELIST === true) && (!in_array(parse_url($url, PHP_URL_HOST), $allowed_urls)))
+		{
+			exit_message('Sorry, downloads from this domain have not been allowed by the administrator');
+		}
+
+		// looks good so far, download the image and make sure it's valid
+		$size = get_headers($url, 1)['Content-Length'];
+		$ext = end(explode('.',$url));
+	}
+
+	// OK, everything checks out so far
+	// check size/ext
+	if ($size > ALLOWED_SIZE)
+	{
+		// file is too big
+		exit_message('Sorry, this file is too big');
+	}
+
+	// size is OK, make sure EXT is allowed
+	if (!in_array($ext, $allowed_ext))
+	{
+		// ext not allowed
+		exit_message('Sorry, this extension is not allowed.');
+	}
+
+	// size and ext are fine
+	// let's set $image to either $_FILES['image'] or $url a[$index]nd check if they're valid
+	if (!is_null($index))
+	{
+		$image = $_FILES['image']['tmp_name'][$index];
+		if (!image) {
+			// image too large.
+			return '';
+		}
+
+		if (!getimagesize($image))
+		{
+			exit_message('Sorry, this does not appear to be a valid image: ' . $_FILES['image']['name'][$index]);
 		}
 	}
-	else if (isset($_POST['url']))
+
+	elseif (!is_null($url))
 	{
-		// set source for thumb
-		$thumb = imagecreatefromstring($image);
+		$image = file_get_contents($url, NULL, NULL, NULL, $size);
+
+		if (!imagecreatefromstring($image))
+		{
+			exit_message('Sorry, this does not appear to be a valid image');
+		}
 	}
 
-	$width = imagesx($thumb);
-	$height = imagesy($thumb);
+	// everything looks good so far! images are valid, size and ext check out
+	// generate an ID, move files and insert into DB
 
-	if ($width > 200 || $height > 200)
+	// generate ID (and make sure it doesn't exist)
+	// prepare query
+	$exists = mysqli_prepare($db, 'SELECT EXISTS(SELECT 1 FROM `images` WHERE `id` = ?)');
+
+	// create ID and check if it exists in the DB
+	do
 	{
-		if ($width > $height)
+		// create ID
+		$id = '';
+		$chars = 'ACDEFHJKLMNPQRTUVWXYZabcdefghijkmnopqrstuvwxyz23479';
+		for ($i = 0; $i < 5; ++$i)
 		{
-			$new_width = 200;
-			// if image height is below 300, don't bother resizing
-			$new_height = floor($height * ($new_width / $width));
+			$id .= $chars[mt_rand(0, 50)];
+		}
+		// $id is now set to a randomly generated ID
+
+		// query DB to see if ID exists
+		mysqli_stmt_bind_param($exists, "s", $id);
+		mysqli_stmt_execute($exists);
+		++$db_queries;
+		mysqli_stmt_bind_result($exists, $result);
+		mysqli_stmt_fetch($exists);
+		mysqli_stmt_close($exists);
+	}
+	while ($result === 1);
+
+	// write image (this is different depending on whether it's an upload or remote download)
+	if (!is_null($index))
+	{
+		$image_path = 'images/' . $id . '.' . $ext;
+
+		// write image
+		move_uploaded_file($image, $image_path);
+	}
+	else if (!is_null($url))
+	{
+		// write image
+		file_put_contents('images/' . $id . '.' . $ext, $image);
+	}
+
+	// create thumbnail (only bother if user is logged in)
+	if (isset($_SESSION['user']))
+	{
+		if (!is_null($index))
+		{
+			// set source for thumb
+			switch ($ext)
+			{
+				case 'jpg':
+					$thumb = imagecreatefromjpeg($image_path);
+				break;
+
+				case 'png':
+					$thumb = imagecreatefrompng($image_path);
+				break;
+
+				case 'gif':
+					$thumb = imagecreatefromgif($image_path);
+				break;
+			}
+		}
+		else if (!is_null($url))
+		{
+			// set source for thumb
+			$thumb = imagecreatefromstring($image);
+		}
+
+		$width = imagesx($thumb);
+		$height = imagesy($thumb);
+
+		if ($width > 200 || $height > 200)
+		{
+			if ($width > $height)
+			{
+				$new_width = 200;
+				// if image height is below 300, don't bother resizing
+				$new_height = floor($height * ($new_width / $width));
+			}
+			else
+			{
+				$new_height = 200;
+				// if image width is below 300, don't bother resizing
+				$new_width = floor($width * ($new_height / $height));
+			}
 		}
 		else
 		{
-			$new_height = 200;
-			// if image width is below 300, don't bother resizing
-			$new_width = floor($width * ($new_height / $height));
+			$new_height = $height;
+			$new_width = $width;
 		}
+
+		$new_thumb = imagecreatetruecolor($new_width, $new_height);
+
+		switch ($ext)
+		{
+			case 'png':
+				imagefill($new_thumb, 0, 0, imagecolorallocate($new_thumb, 255, 255, 255));
+				imagealphablending($new_thumb, TRUE);
+			break;
+
+			case 'gif':
+				$new_thumb = imagecolorallocate($thumb, 0, 0, 0);
+				imagecolortransparent($thumb, $new_thumb);
+			break;
+		}
+		
+		imagecopyresized($new_thumb, $thumb, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+		imagedestroy($thumb);	
+
+		imagejpeg($new_thumb, 'thumbs/' . $id . '.jpg', 30);
+		imagedestroy($new_thumb);
+	}
+
+	// check if user is logged in or not and write info to DB
+	if (!isset($_SESSION['user']))
+	{
+		$query = mysqli_prepare($db, 'INSERT INTO `images` (`id`, `ext`, `ip`) VALUES (?, ?, ?)');
+		mysqli_stmt_bind_param($query, 'sss', $id, $ext, $ip);
 	}
 	else
 	{
-		$new_height = $height;
-		$new_width = $width;
+		$query = mysqli_prepare($db, 'INSERT INTO `images` (`id`, `ext`, `user`, `ip`) VALUES (?, ?, ?, ?)');
+		mysqli_stmt_bind_param($query, 'ssis', $id, $ext, $user, $ip);
 	}
 
-	$new_thumb = imagecreatetruecolor($new_width, $new_height);
+	// set data for query
+	$user = $_SESSION['user'];
+	$ip = $_SERVER['REMOTE_ADDR'];
 
-	switch ($ext)
-	{
-		case 'png':
-			imagefill($new_thumb, 0, 0, imagecolorallocate($new_thumb, 255, 255, 255));
-			imagealphablending($new_thumb, TRUE);
-		break;
+	// insert data
+	mysqli_stmt_execute($query);
+	++$db_queries;
+	mysqli_stmt_close($query);
 
-		case 'gif':
-			$new_thumb = imagecolorallocate($thumb, 0, 0, 0);
-			imagecolortransparent($thumb, $new_thumb);
-		break;
+	return $id;
+}
+
+$size = empty($_FILES['image']['name'][0]) ? 0 : count($_FILES['image']['name']);
+$multiple = $size > 0;
+
+if ($size > 0) {
+	for($i = 0; $i < $size; $i++) {
+		$id = process_upload($i, null);
+		// echo 'upload: ' . $id . '<br>';
 	}
-	
-	imagecopyresized($new_thumb, $thumb, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
-	imagedestroy($thumb);	
-
-	imagejpeg($new_thumb, 'thumbs/' . $id . '.jpg', 30);
-	imagedestroy($new_thumb);
+} else {
+	process_upload(null, $url);
 }
-
-// check if user is logged in or not and write info to DB
-if (!isset($_SESSION['user']))
-{
-	$query = mysqli_prepare($db, 'INSERT INTO `images` (`id`, `ext`, `ip`) VALUES (?, ?, ?)');
-	mysqli_stmt_bind_param($query, 'sss', $id, $ext, $ip);
-}
-else
-{
-	$query = mysqli_prepare($db, 'INSERT INTO `images` (`id`, `ext`, `user`, `ip`) VALUES (?, ?, ?, ?)');
-	mysqli_stmt_bind_param($query, 'ssis', $id, $ext, $user, $ip);
-}
-
-// set data for query
-$user = $_SESSION['user'];
-$ip = $_SERVER['REMOTE_ADDR'];
-
-// insert data
-mysqli_stmt_execute($query);
-++$db_queries;
-mysqli_stmt_close($query);
 
 // close connection
 mysqli_close($db);
 
-header('location: ' . VIEW_URL . $id);
+if ($multiple) {
+	header('location: ' . SITE_URL . '/account.php');
+} else {
+	header('location: ' . VIEW_URL . $id);
+}
