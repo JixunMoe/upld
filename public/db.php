118a7215f4c5752a3b93643ba4d63d112dbfa183
Jixun Wu <jixun.moe@gmail.com>
2019-01-23 18:03:34 +0000

    display error message if connect failed

--- public/db.php
+++ public/db.php
@@ -1,4 +1,9 @@
 <?php
 
-$db = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME, DB_PORT);
+$db = @mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME, DB_PORT);
 
+require_once "common.php";
+
+if (!$db) {
+    exit_message("Failed to establish connection to the database.");
+}
