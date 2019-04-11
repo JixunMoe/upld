a6fa5d29f148394e4f28a0150a6ea914d8cc1e14
Jixun Wu <jixun.moe@gmail.com>
2019-04-11 03:03:12 +0100

    fix id check for report and view

--- public/view.php
+++ public/view.php
@@ -3,7 +3,7 @@
 require('config.php');
 require('common.php');
 
-if (!ctype_alnum($_GET['id']) || (strlen($_GET['id']) !== 5))
+if (!ctype_alnum($_GET['id']))
 {
 	exit_message('Oops, that ID appears to be invalid. IDs should have 5 characters and contain letters and numbers only.');
 }
